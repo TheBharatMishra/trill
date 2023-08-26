4e981b869608a833383fb86bb04fe8a0e5972325
TheBharatMishra <mishrabm2004@gmail.com>
2023-08-26 06:21:21 +0530

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,10 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.posts.getAll.useQuery();
 
   const user = useUser();
+  console.log(hello.data);
 
   return (
     <>
@@ -16,8 +18,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!user.isSignedIn && <SignInButton />}
-        {!!user.isSignedIn && <SignOutButton />}
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div className="flex items-center justify-center">
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
